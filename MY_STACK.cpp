7254842e51df1d354b31e161cb5bceca540e31ad
Bound <dope@mail.gov>
2017-08-29 21:07:48 +0300

    added functionality

--- MY_STACK.cpp
+++ MY_STACK.cpp
@@ -1,1 +1,20 @@
-#include "MY_STACK.h"
+//#include "MY_STACK.h"
+#include <cassert>
+
+template <typename T>
+void MY_STACK<T>::push(const T&& val)
+{
+    assert(counter_ < LIMIT);
+    head_ = new Node(val,head_);
+    ++counter_;
+}
+
+template <typename T>
+void MY_STACK<T>::pop()
+{
+    assert(counter_ != 0);
+    Node* old = head_;
+    head_ = head_->next_;
+    delete old;
+    --counter_;
+}
--- MY_STACK.h
+++ MY_STACK.h
@@ -1,35 +1,61 @@
 #ifndef MY_STACK_H
 #define MY_STACK_H
-
+#include <stdexcept>
 template <typename T>
 class MY_STACK
 {
-    public:
-        MY_STACK() : head_(nullptr), counter_(0) 
-		{}
-        ~MY_STACK()
-		{
-    Node* node = head_;
-    Node* next;
+private:
 
-    while (node) {
-        next = node->next_;
-        delete node;
-        node = next;
-    }
-		}
-
-    private:
-
-    struct Node {
+    struct Node
+    {
         T value_;
         Node* next_;
 
-        Node(T& elem) : value_(elem), next_(nullptr)
+        Node(const T& elem, Node* link = nullptr)
+            : value_(elem), next_(link)
         {}
     };
-    unsigned counter_;
     Node* head_;
-};
+    unsigned counter_;
+    unsigned const LIMIT = 5;
 
+public:
+    MY_STACK() : head_(nullptr), counter_(0)
+    {}
+    ~MY_STACK()
+    {
+        Node* node = head_;
+        Node* next;
+
+        while (node)
+        {
+            next = node->next_;
+            delete node;
+            node = next;
+        }
+    }
+    void push(const T&& val);
+    void pop();
+    void top()
+    {
+        if (empty())
+        {
+            throw std::out_of_range("underflow");
+        }
+        std::cout << head_->value_ << std::endl;
+    }
+    bool empty() const
+    {
+        return counter_ == 0;
+    }
+    unsigned size() const
+    {
+        return counter_;
+    }
+    bool full() const
+    {
+        return counter_ == LIMIT;
+    }
+};
+#include "MY_STACK.cpp"
 #endif // MY_STACK_H
